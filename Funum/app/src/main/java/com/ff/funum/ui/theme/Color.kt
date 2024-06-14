ec0e958dcda6ba2fcc940e9ca75a4ab3905ceb93
VaM03 <00121522@uca.edu.sv>
2024-06-13 19:31:18 -0600

    :rocket: navbar implementation

--- Funum/app/src/main/java/com/ff/funum/ui/theme/Color.kt
+++ Funum/app/src/main/java/com/ff/funum/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Paleta de Colores
+val DarkGreen = Color(0xFF024029)
+val Green = Color(0xFF025939)
+val GreenTopics = Color(0xFF7CA697)
+val LightGreen = Color(0xFFC1D9D2)
+val White = Color(0xFFFFFFFF)
+val LightGray = Color(0xFFD9D9D9)
+val Black = Color(0xFF000000)
