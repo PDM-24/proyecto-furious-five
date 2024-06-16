947f8ec156c33b842cdfbf70333dda1f2785ec17
Joma203 <00120422@uca.edu.sv>
2024-06-16 13:02:56 -0600

    add: update topic screen

--- Funum/app/src/main/java/com/ff/funum/screens/LessonsViewModel.kt
+++ Funum/app/src/main/java/com/ff/funum/screens/LessonsViewModel.kt
@@ -1,9 +1,13 @@
 package com.ff.funum.screens
 
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresExtension
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ff.funum.data.api.ApiClient
+import com.ff.funum.data.api.TopicAPI
+import com.ff.funum.data.api.UpdateTopic
 import com.ff.funum.data.listLessons
 import com.ff.funum.utils.Constants
 import kotlinx.coroutines.Dispatchers
@@ -27,6 +31,50 @@ class LessonsViewModel: ViewModel() {
                         Log.i("MainViewModel", e.toString())
                     }
 
+                    else -> {
+                        Log.i("MainViewModel", e.toString())
+                    }
+                }
+            }
+        }
+    }
+
+    //Gestiona la informacion de Update topic screen
+    var updatedTopic :TopicAPI= TopicAPI()
+
+    //Update topic
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun updateTopic(topic: TopicAPI, idLesson:String, idTopic:String="",token:String) {
+        viewModelScope.launch(Dispatchers.IO){
+            try {
+                val response = topic.id?.let {
+                    UpdateTopic(topic.nombre,topic.contenido,topic.ponderacion,topic.visibility,topic.imagen,
+                        it,idLesson)
+                }?.let { api.updateTopic(it, idTopic, "Bearer $token") }
+                Log.i("MainViewModel",response.toString())
+            }catch (e:Exception){
+                when(e){
+                    is retrofit2.HttpException -> {
+                        e.message?.let { Log.i("MainViewmodel", it) }
+                    }
+                    else -> {
+                        Log.i("MainViewModel", e.toString())
+                    }
+                }
+            }
+        }
+    }
+    //Toggle topic visibility
+    fun toggleTopicVisibility(idTopic:String="",token:String) {
+        viewModelScope.launch(Dispatchers.IO){
+            try {
+                val response = api.toggleTopicVisibility( idTopic, "Bearer $token")
+                Log.i("MainViewModel",response.toString())
+            }catch (e:Exception){
+                when(e){
+                    is retrofit2.HttpException -> {
+                        e.message?.let { Log.i("MainViewmodel", it) }
+                    }
                     else -> {
                         Log.i("MainViewModel", e.toString())
                     }
