276b14991ce85a42954cf7a4f9d44da2937ef9e1
Ricardo Garcia <00148922@uca.edu.sv>
2024-06-17 18:22:08 -0600

    fix: LoginData variables

--- Funum/app/src/main/java/com/ff/funum/model/LoginData.kt
+++ Funum/app/src/main/java/com/ff/funum/model/LoginData.kt
@@ -1,15 +1,12 @@
 package com.ff.funum.model
 
+import com.ff.funum.utils.Constants
 import com.google.gson.annotations.SerializedName
 
 data class LoginData (
-    @SerializedName(value = "identifier")
+    @SerializedName(value = Constants.LOGIN_IDENTIFIER)
     var identifier: String = "",
 
-    @SerializedName(value = "password")
+    @SerializedName(value = Constants.LOGIN_PWD)
     var pwd: String = ""
-){
-    fun isNotEmpty(): Boolean {
-        return identifier.isNotEmpty() && pwd.isNotEmpty()
-    }
-}
+)
