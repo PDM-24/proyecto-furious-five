83a4c3b2cfce7b76424d216a6f052c6a30b2b423
Joma203 <00120422@uca.edu.sv>
2024-06-21 13:28:21 -0600

    add: avatar controller

---
+++ Server/controllers/avatar.controller.js
@@ -0,0 +1,42 @@
+const Avatar = require("../models/avatar.model");
+
+const controller = {};
+
+controller.findAll = async (req, res, next)=>{
+    try {
+       const avatar = await Avatar.find()
+       return res.status(200).json({avatar});
+    } catch (error) {
+
+        next(error);
+    }
+}
+controller.save = async (req, res, next)=>{
+    try {
+        const { costo, nombre, imagen} = req.body;
+        const {identifier} = req.params;
+
+
+        let avatar = await Avatar.find({imagen: imagen});
+
+        if(!avatar){
+            avatar = new Avatar();
+        }else{
+            return res.status(409).json({error: "El avatar ya existe"});
+        }
+
+        avatar["costo"]=costo;
+        avatar["nombre"]=nombre;
+        avatar["imagen"]=imagen;
+
+       const avatarSaved = await avatar.save();
+
+       if(!avatarSaved){
+        return res.status(409).json({error: "Error creating lesson"});
+       }
+       return res.status(201).json(avatarSaved);
+    } catch (error) {
+
+        next(error);
+    }
+}
